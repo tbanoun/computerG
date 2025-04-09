6370f64dc412e5d5cc74fa6d638b665e016f1ea0
tbanoun <taharbanoun06@gmail.com>
2025-04-09 21:19:21 +0100

    Add the fields Delivery Time Remote Stock and Stock, then display their respective values on the web pages.

--- web_custumize_computerG/models/product_template.py
+++ web_custumize_computerG/models/product_template.py
@@ -5,3 +5,5 @@ class ProductTemplate(models.Model):
 
 
     showDelivryMessage = fields.Boolean(default=True)
+    messageDelivryTimeRemoteStock = fields.Char('Delivery Time – Remote Stock Message', default='Ship 4-8 Days')
+    messageDelivryTimeStock = fields.Char('Delivery Time – Stock Message', default='Ship 1-2 Days')
