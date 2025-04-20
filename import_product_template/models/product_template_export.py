63fc8a860afa3a82c0ba10cd683476d5a1f50a42
tbanoun <taharbanoun06@gmail.com>
2025-04-20 07:43:13 +0100

    update

--- import_product_template/models/product_template_export.py
+++ import_product_template/models/product_template_export.py
@@ -75,18 +75,18 @@ class ProductTemplateExport(models.Model):
                 category_xmld_id or '',  # categ_id/id
                 product.default_code or "",  # default_code
                 product.barcode or "",  # barcode
-                # product.x_product_website_url,  # x_product_website_url (empty in example)
-                # product.x_CPU,  # x_CPU (empty in example)
-                # product.x_sreen_size,  # x_sreen_size (empty in example)
-                # product.x_hddtype,  # x_hddtype (empty in example)
-                # product.x_ram,  # x_ram (empty in example)
-                # product.x_GPU,  # x_GPU (empty in example)
-                # "", # f"__export__.res_partner_{product.manufacturer.id}_{product.manufacturer.id}" if product.manufacturer else "",
-                # # manufacturer_id
-                # product.x_kind,  # x_kind (empty in example)
-                # product.x_condition,  # x_condition (empty in example)
-                # product.x_,  # x_ (empty in example)
-                # product.image_url,  # image_url (you can add product.image_1920 URL here)
+                product.x_product_website_url,  # x_product_website_url (empty in example)
+                product.x_CPU,  # x_CPU (empty in example)
+                product.x_sreen_size,  # x_sreen_size (empty in example)
+                product.x_hddtype,  # x_hddtype (empty in example)
+                product.x_ram,  # x_ram (empty in example)
+                product.x_GPU,  # x_GPU (empty in example)
+                "", # f"__export__.res_partner_{product.manufacturer.id}_{product.manufacturer.id}" if product.manufacturer else "",
+                # manufacturer_id
+                product.x_kind,  # x_kind (empty in example)
+                product.x_condition,  # x_condition (empty in example)
+                product.x_,  # x_ (empty in example)
+                product.image_url,  # image_url (you can add product.image_1920 URL here)
                 public_categ_ids or '', # public_categ_ids/id
                 product.available_in_pos or '', # available_in_pos
                 pos_categ_id or '', # pos_categ_id/id
