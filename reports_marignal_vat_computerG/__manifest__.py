a67b86cce505e38a105d360c72c374cbb55b5034
tbanoun <taharbanoun06@gmail.com>
2025-04-30 08:34:18 +0100

    create a new report for print invoice marginal VAT

---
+++ reports_marignal_vat_computerG/__manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    'name': 'Reports Marginal VAT',
+    'description': '',
+    'summary': '',
+    'category': 'Extra Tools',
+    'version': '16.0.0.0.1',
+    'depends': ['sale', 'account'],
+    'license': 'OPL-1',
+    'author': 'Tahar BANOUN',
+    'company': 'https://digitalai.academy/',
+    'maintainer': 'Tahar BANOUN',
+    'website': 'https://digitalai.academy/',
+    'images': [
+        'static/description/icon.png',
+    ],
+    'data': [
+        # 'reports/sale_order_inherit.xml',
+        'reports/invoice_inherit.xml',
+    ],
+}
