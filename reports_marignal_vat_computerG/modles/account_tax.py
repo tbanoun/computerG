75d1da1b91e9e0935c47981e6a9bf89f8fc98f3c
tbanoun <taharbanoun06@gmail.com>
2025-05-02 09:28:09 +0100

    update invoice

--- reports_marignal_vat_computerG/modles/account_tax.py
+++ reports_marignal_vat_computerG/modles/account_tax.py
@@ -5,3 +5,23 @@ class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     showOnInvoice = fields.Boolean('Is Marginal VAT')
+
+
+
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    price_tax = fields.Monetary(
+        string='Total',
+        compute='_compute_totals_tax', store=True,
+        currency_field='currency_id',
+    )
+
+    @api.depends('price_subtotal', 'price_subtotal')
+    def _compute_totals_tax(self):
+        for line in self:
+            price_total = line.price_total or 0
+            price_subtotal = line.price_subtotal or 0
+            line.price_tax = price_total - price_subtotal
