730a7b1a8e9fffd8d1211aa4f4eb5ce495639d27
tbanoun <taharbanoun06@gmail.com>
2025-06-09 08:40:19 +0100

    update widht delivery message

--- web_custumize_computerG/static/src/js/variant_mixin.js
+++ web_custumize_computerG/static/src/js/variant_mixin.js
@@ -22,7 +22,8 @@ odoo.define('web_custumize_computerG.VariantMixin', function (require) {
                 infoMessageEl.style.setProperty('font-weight', '500', 'important');
                 infoMessageEl.style.setProperty('background', 'rgba(246, 242, 250, 0.5)', 'important');
                 infoMessageEl.style.setProperty('padding', '0.4rem 0.6rem', 'important');
-                infoMessageEl.style.setProperty('width', 'fit-content', 'important');
+//                infoMessageEl.style.setProperty('width', 'fit-content', 'important');
+                infoMessageEl.style.setProperty('width', '503px', 'important');
                 infoMessageEl.style.setProperty('border-radius', '4px', 'important');
                 infoMessageEl.style.setProperty('color', '#6F1152', 'important');
                 infoMessageEl.style.setProperty('font-size', '10pt', 'important');
@@ -46,7 +47,8 @@ odoo.define('web_custumize_computerG.VariantMixin', function (require) {
                 infoMessageEl.style.setProperty('font-weight', '500', 'important');
                 infoMessageEl.style.setProperty('background', 'rgba(242, 250, 246, 0.5)', 'important'); // F2FAF6 avec 50% d'opacité
                 infoMessageEl.style.setProperty('padding', '0.4rem 0.6rem', 'important');
-                infoMessageEl.style.setProperty('width', 'fit-content', 'important');
+//                infoMessageEl.style.setProperty('width', 'fit-content', 'important');
+infoMessageEl.style.setProperty('width', '503px', 'important');
                 infoMessageEl.style.setProperty('border-radius', '4px', 'important');
                 infoMessageEl.style.setProperty('color', '#116F41', 'important');
                 infoMessageEl.style.setProperty('font-size', '10pt', 'important');
@@ -78,7 +80,8 @@ odoo.define('web_custumize_computerG.VariantMixin', function (require) {
                 infoMessageEl.style.setProperty('font-weight', '600', 'important'); // Poppins Semi Bold
                 infoMessageEl.style.setProperty('background', 'rgba(255, 232, 238, 0.5)', 'important'); // #FFFCEE avec 50% opacité
                 infoMessageEl.style.setProperty('padding', '0.4rem 0.6rem', 'important');
-                infoMessageEl.style.setProperty('width', 'fit-content', 'important');
+//                infoMessageEl.style.setProperty('width', 'fit-content', 'important');
+infoMessageEl.style.setProperty('width', '503px', 'important');
                 infoMessageEl.style.setProperty('border-radius', '4px', 'important');
                 infoMessageEl.style.setProperty('color', '#F45454', 'important');
                 infoMessageEl.style.setProperty('font-size', '10pt', 'important');
